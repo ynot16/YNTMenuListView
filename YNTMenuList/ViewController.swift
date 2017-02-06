6885da776fc9e89d6a395cf530e357e8709c56c9
ynot16 <695110728@qq.com>
2017-02-06 10:33:09 +0800

    Initial Commit

---
+++ YNTMenuList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YNTMenuList
+//
+//  Created by bori－applepc on 2017/2/6.
+//  Copyright © 2017年 bori－applepc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
